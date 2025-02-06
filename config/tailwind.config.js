97d03cb993b3ba1b8eb92db14d1087f877a512ec
zeshan <zeshanahmedoff@gmail.com>
2025-02-06 09:51:04 +0500

    devise auth added with customized tailwind styling

---
+++ config/tailwind.config.js
@@ -0,0 +1,38 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}',
+    './app/components/**/*.{erb,haml,html,slim}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        homeytheme: {
+          primary: '#7C5BFB',
+          secondary: '#BFAEFD',
+          accent: '#9333EA',
+          neutral: '#1E293B',
+          'base-100': '#FFFFFF',
+          info: '#3B82F6',
+          success: '#22C55E',
+          warning: '#EAB308',
+          error: '#EF4444',
+        },
+      },
+      'light',
+      'dark',
+    ],
+  },
+};
